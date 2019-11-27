df46fbc8374dccbc333610da14e1ffd60cd8c63f
ahmedElmaghr <elmaghraoui.ahmed@gmail.com>
2019-11-27 20:52:50 +0100

    Updates

---
+++ precache-manifest.4f0d922b96b631b271285f0926ce3ad8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c0fd7e8f6c3655610e1ab88b409d0d",
+    "url": "/mediaFrancaisDev/index.html"
+  },
+  {
+    "revision": "cb17c1a00036f968bf11",
+    "url": "/mediaFrancaisDev/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "ce2f588ee03ee1bdbf30",
+    "url": "/mediaFrancaisDev/static/css/main.8cc8da5a.chunk.css"
+  },
+  {
+    "revision": "cb17c1a00036f968bf11",
+    "url": "/mediaFrancaisDev/static/js/2.39ac959b.chunk.js"
+  },
+  {
+    "revision": "ce2f588ee03ee1bdbf30",
+    "url": "/mediaFrancaisDev/static/js/main.c3c9c1ac.chunk.js"
+  },
+  {
+    "revision": "22ac011821cd4506f66c",
+    "url": "/mediaFrancaisDev/static/js/runtime-main.0158bd43.js"
+  },
+  {
+    "revision": "9f4f50474fdf445d0f992923a4fade5f",
+    "url": "/mediaFrancaisDev/static/media/countries.9f4f5047.tsv"
+  },
+  {
+    "revision": "d67865902b1d454a35d97dc8bf910c9f",
+    "url": "/mediaFrancaisDev/static/media/medias_francais_mock.d6786590.tsv"
+  },
+  {
+    "revision": "d49ff9ffc649a7eea05611350f4dc369",
+    "url": "/mediaFrancaisDev/static/media/relations_medias_francais.d49ff9ff.tsv"
+  }
+]);
