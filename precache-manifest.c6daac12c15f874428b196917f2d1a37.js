b84a695378a7247f992b9ad29495001358640040
ahmedElmaghr <elmaghraoui.ahmed@gmail.com>
2019-12-02 00:07:44 +0100

    Updates

---
+++ precache-manifest.c6daac12c15f874428b196917f2d1a37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec6ee42b04b66124bbdeda93e84377b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e8a164eb6e4b51197a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6a6923bdc73bd419ebb5",
+    "url": "/static/css/main.8cc8da5a.chunk.css"
+  },
+  {
+    "revision": "c7e8a164eb6e4b51197a",
+    "url": "/static/js/2.4275492b.chunk.js"
+  },
+  {
+    "revision": "6a6923bdc73bd419ebb5",
+    "url": "/static/js/main.c8fcefa3.chunk.js"
+  },
+  {
+    "revision": "52de288e8fa73801b309",
+    "url": "/static/js/runtime-main.d46fe643.js"
+  },
+  {
+    "revision": "9f4f50474fdf445d0f992923a4fade5f",
+    "url": "/static/media/countries.9f4f5047.tsv"
+  },
+  {
+    "revision": "d67865902b1d454a35d97dc8bf910c9f",
+    "url": "/static/media/medias_francais_mock.d6786590.tsv"
+  },
+  {
+    "revision": "d49ff9ffc649a7eea05611350f4dc369",
+    "url": "/static/media/relations_medias_francais.d49ff9ff.tsv"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mediaFrancaisDev/precache-manifest.4c147ef706ca97a860cadcfcd22fb662.js"
+  "/precache-manifest.c6daac12c15f874428b196917f2d1a37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mediaFrancaisDev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
