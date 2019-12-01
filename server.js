3be8f5fb6a4b5d8f1ffb2ec1d480c1d897a1dd38
ahmedElmaghr <elmaghraoui.ahmed@gmail.com>
2019-12-01 20:29:34 +0100

    fix server.js

--- server.js
+++ server.js
@@ -1,73 +1,76 @@
-var express = require('express');
-var app = express();
-var multer = require('multer')
-//const publicPath = path.join(__dirname, 'client','build');
+const express = require('express');
+const app = express();
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const fileType = require('file-type');
+const bluebird = require('bluebird');
+const multiparty = require('multiparty');
 var cors = require('cors');
-
-
-
-
-app.use(cors())
-
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-    cb(null, 'client/public')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' +file.originalname )
-  }
-})
-if(process.env.NODE_ENV === "production" ){
-  console.log("mode production");
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*',(req,res)=>{
-    //console.log("res",res)
-    console.log("file sent",path.resolve(__dirname, 'client', 'build', 'index.html'))
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+/*
+app.use(cors);
+app.options('*',cors);
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
 }
-/*
-app.get('*', (req, res) => {
-  console.log("res",res)
-  console.log("file sent",path.join(publicPath, 'index.html'))
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+app.use(allowCrossDomain);
 */
-
-var upload = multer({ storage: storage }).single('file');
-/*
-const router = express.Router();
-router.post('/upload',function(req, res) {
-  console.log("test") 
-  upload(req, res, function (err) {
-    upload(req, res, (err) => {
-      console.log("Request ---", req.body);
-      console.log("Request file ---", req.file);//Here you get file.
-      //Now do where ever you want to do
-      if(!err)
-         return res.send(200).end();
-  })
-});
-*/
-
-app.post('/upload',function(req, res) {
-    console.log("call server upload post") 
-    upload(req, res, function (err) {
-           if (err instanceof multer.MulterError) {
-               return res.status(500).json(err)
-           } else if (err) {
-               return res.status(500).json(err)
-           }
-      return res.status(200).send(req.file)
-
-    })
-
+// configure the keys for accessing AWS
+AWS.config.update({
+  accessKeyId : 'AKIAXRYQHWPRH6FP64NK',
+  secretAccessKey : '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'
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, function() {
 
-    console.log(`App running on port ${PORT}`);
+// configure AWS to work with promises
+AWS.config.setPromisesDependency(bluebird);
 
+// create S3 instance
+const s3 = new AWS.S3();
+
+
+// Define POST route
+app.post('/test-upload', (request, response) => {
+  console.log("call /test-upload from server")
+  const form = new multiparty.Form();
+    form.parse(request, async (error, fields, files) => {
+      if (error) throw new Error(error);
+      try {
+      const path = files.file[0].path;
+      const data = await uploadFile(path);
+      console.log("data",data);
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+    });
 });
+
+const uploadFile = (path) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(path);
+  const timestamp = Date.now().toString();
+  const fileName = `wordmap-data-${timestamp}.tsv`;
+  // Setting up S3 upload parameters
+  const params = {
+      Bucket: 'worldmap-ocp',
+      Key: fileName, // File name you want to save as in S3
+      Body: fileContent
+  };
+  console.log("params",params);
+  // Uploading files to the bucket
+ return s3.upload(params, function(err, data) {
+      if (err) {
+        console.log(err);
+          throw err;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+  }).promise();
+};
+
+app.listen(process.env.PORT || 9000);
+console.log('Server up and running...');
+
+//test('test.tsv')
