10aae714044c542a32e3c833be5dd0806bcd2645
ELMAGHRAOUI <ahmed.2.elmaghraoui.external@atos.net>
2020-01-14 10:13:46 +0100

    add mysql local configuration

--- server.js
+++ server.js
@@ -1,138 +1,35 @@
-const express = require('express');
-var multer = require('multer')
-const logger = require('morgan');
-const app = express();
-const AWS = require('aws-sdk');
-const fs = require('fs');
-const bluebird = require('bluebird');
-const multiparty = require('multiparty');
 const cors = require('cors');
-const CACHE_FILE_URL = "./client/src/db/cache/my.db";
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
+var connString = buildStringCnx(
+    process.env.MYSQL_HOST_IP,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    process.env.MYSQL_DATABASE);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+var conn = mysql.createConnection(connString);
 
-var Datastore = require('nedb');
-//var users = new Datastore({ filename: `${process.env.TMP}` });
+function buildStringCnx(host, user, password, database) {
+    var connString = `mysql://${user}:${password}@${host}/${database}?charset=utf8_general_ci&timezone=-0700`;
+    console.log(connString);
+    return connString;
+}
+app.use(cors());
 
-var users = new Datastore({ filename: CACHE_FILE_URL, autoload: true });
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
 
-app.post('/db/persist', (req, res) => {  
-  
-  fs.writeFile(CACHE_FILE_URL, '', function(){console.log('init cache done')})
-  var data = {
-    data : req.body.data
-  };
-  users.insert(data, function(err, doc) {
-    if(err){
-      return res.status(500).json(err);
-    }
-  });
-  
-  return res.status(200).send("file  successfully cached");
-
-});
-
-app.get("/db/all",(req,res)=>{
-  console.log("/db/all");
-  users.find({}, function(err, docs) {
-    console.log("docs",docs);
-    docs.forEach(function(d) {
-        console.log('Found user:', d.name);
+app.get('/test', (req, res) => {
+    const { table } = req.query;
+    conn.query(`select * from ${table}`, (err, results) => {
+        console.log(`select * from ${table}`);
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        } else {
+            return res.send(results);
+        }
     });
 });
-
-});
-
-if(process.env.NODE_ENV === "production" ){
-  console.log("mode production");
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*',(req,res)=>{
-    //console.log("res",res)
-    console.log("file sent",path.resolve(__dirname, 'client', 'build', 'index.html'))
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
-
-//LOCAL
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-  cb(null, 'client/public')
-},
-filename: function (req, file, cb) {
-  cb(null, Date.now() + '-' +file.originalname )
-}
-})
-
-var uploadLocal = multer({ storage: storage }).single('file');
-//
-app.post('/upload_localy',function(req, res) {
-    console.log("call server upload post") 
-    uploadLocal(req, res, function (err) {
-           if (err instanceof multer.MulterError) {
-               return res.status(500).json(err)
-           } else if (err) {
-               return res.status(500).json(err.response)
-           }
-      return res.status(200).send(req.file)
-
-    })
-
-});
-
-// configure the keys for accessing AWS
-AWS.config.update({
-  accessKeyId : process.env.REACT_APP_S3AccessKeyID,
-  secretAccessKey : process.env.REACT_APP_S3SecretAccessKey,
-});
-
-// configure AWS to work with promises
-AWS.config.setPromisesDependency(bluebird);
-
-// create S3 instance
-const s3 = new AWS.S3();
-
-// Define POST route
-app.post('/upload_cloud', (request, response) => {
-  const form = new multiparty.Form();
-    form.parse(request, async (error, fields, files) => {
-      if (error) throw new Error(error);
-      try {
-      const path = files.file[0].path;
-      //Updloading files 
-      const data = await uploadFile(path);
-      console.log("data",data);
-      return response.status(200).send(data);
-    } catch (error) {
-      return response.status(400).send(error);
-    }
-    });
-});
-
-const uploadFile = (path) => {
-  // Read content from the file
-  const fileContent = fs.readFileSync(path);
-  const timestamp = Date.now().toString();
-  const fileName = `wordmap-data-${timestamp}.tsv`;
-  // Setting up S3 upload parameters
-  const params = {
-      Bucket: 'worldmap-ocp',
-      Key: fileName, // File name you want to save as in S3
-      Body: fileContent
-  };
-  console.log("params",params);
-  // Uploading files to the bucket
- return s3.upload(params, function(err, data) {
-      if (err) {
-        console.log(err);
-          throw err;
-      }
-      console.log(`File uploaded successfully. ${data.Location}`);
-  }).promise();
-};
-
-app.listen(process.env.PORT || 4000);
-console.log('Server up and running...');
